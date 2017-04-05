ad2eb68c2814ca9b1d81c38bed4892835b0cc651
Matheus <matheus511377@hotmail.com>
2017-04-04 23:05:45 -0300

    Main Actitity

--- app/src/main/java/com/matheus/jogodedamas/MainActivity.java
+++ app/src/main/java/com/matheus/jogodedamas/MainActivity.java
@@ -1,7 +1,11 @@
 package com.matheus.jogodedamas;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        int teste = getStringIdentifier(this,"a1");
+        ImageView img = (ImageView) findViewById(teste);
+
     }
+
+    public static int getStringIdentifier(Context pContext, String pString){
+        return pContext.getResources().getIdentifier(pString, "id", pContext.getPackageName());
+    }
+
+
 }
